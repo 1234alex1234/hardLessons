3a8e083e64b2ad423875d681138a5b5ad2f7a526
1234alex1234 <verygoog@yandex.ru>
2022-02-06 22:07:06 +0300

    Домашнее задание номер 5

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-function noName(text) {
-  let maxLength = 30;
-  if(typeof text === 'string') {
-    text = text.trim();
-    if(text.length > maxLength) {
-      text = text.substring(0, maxLength) + '...';
-      console.log(text);
-    } else {
-      console.log(text);
-    }
-  } else {
-    console.log("Введите пожалуйста строку или хуже будет!");
+//Задание номер 1
+let arr = ['12', '45', '67', '789', '40', '21', '241'];
+
+for(let i = 0; i< arr.length; i++) {
+  if(arr[i][0] == 2 || arr[i][0] == 4) {
+    console.log(arr[i]);
   }
 }
 
-noName("                    Привет!Я очень длинный текст возможно даже длиннее 30 символов!                ");
-noName("                    привет!А я не очень!               ");
-noName(1);
+// Задание номер 2
+let num = 100;
 
+for(let i = 2; i < num; i++) {
+  let bool = true;
+      
+  for(let j = 3; j < Math.sqrt(num); j++) {
+    if(i % j == 0) {
+      bool = false;
+      break;
+    } 
+  } 
 
-
+  if(bool == true && i % 2 != 0 || (i == 3 || i== 5 || i== 7)) {
+    console.log(`${i}: Делители данного числа 1 и ${i}`);
+  }
+}
 
