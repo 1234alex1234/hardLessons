402483bd0b120f6f0cfae6850dfb83177e67025d
1234alex1234 <verygoog@yandex.ru>
2022-02-11 21:46:02 +0300

    Исправил дз 7

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 let array = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+let out = document.querySelector('.out');
 
 for (let i = 0; i < array.length; i++) {
-  const d = new Date();
+  const day = new Date();
 
-  if (i < 5 && array[i] !== (array[d.getDay() - 1])) {
-    document.querySelector('.out').innerHTML += `${array[i]}<br>`;
-  } else if (i > 4 && array[i] !== (array[d.getDay() - 1])) {
-    document.querySelector('.out').innerHTML += `<i>${array[i]}</i><br>`;
+  if (i < 5 && array[i] !== (array[day.getDay() - 1])) {
+    out.innerHTML += `${array[i]}<br>`;
+  } else if (i > 4 && array[i] !== (array[day.getDay() - 1])) {
+    out.innerHTML += `<i>${array[i]}</i><br>`;
   }
 
-  if (array[d.getDay() - 1] == array[i]) {
-    document.querySelector('.out').innerHTML += `${array[i].bold()}<br>`;
+  if (array[day.getDay() - 1] == array[i]) {
+    out.innerHTML += `<b>${array[i]}</b><br>`;
   }
 }
