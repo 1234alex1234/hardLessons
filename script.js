68ac8556a49821ad20ea8e28cf64aaa928cb314e
1234alex1234 <verygoog@yandex.ru>
2022-02-14 23:01:17 +0300

    Выполненный урок номер 9

--- script.js
+++ script.js
@@ -1,16 +1,73 @@
-let array = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
 let out = document.querySelector('.out');
+let out1 = document.querySelector('.out-1');
+let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+let arrMonth = ['Декабря', 'Января', 'Февраля', 'Марта', 'Апреля', 'Мая',
+  'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября'
+];
 
-for (let i = 0; i < array.length; i++) {
-  const day = new Date();
+function getFullTime1() {
+  let nowDate = new Date();
+  let textSecond = 'секунд';
+  let textMinutes = 'минут';
+  let textHours = 'час';
+  let year = nowDate.getFullYear();
+  let month = arrMonth[nowDate.getMonth() + 1];
+  let day = nowDate.getDate();
+  let textDay = days[(nowDate.getDay())];
+  let hours = nowDate.getHours();
+  let minutes = nowDate.getMinutes();
+  let seconds = nowDate.getSeconds();
+  let s = seconds % 10;
+  let m = minutes % 10;
 
-  if (i < 5 && array[i] !== (array[day.getDay() - 1])) {
-    out.innerHTML += `${array[i]}<br>`;
-  } else if (i > 4 && array[i] !== (array[day.getDay() - 1])) {
-    out.innerHTML += `<i>${array[i]}</i><br>`;
+
+  if (s == 1 && seconds !== 11) {
+    textSecond = textSecond + 'а';
+  } else if (seconds == 12 || seconds == 13 || seconds == 14) {
+    textSecond = textSecond + '';
+  } else if (s >= 2 && s <= 4) {
+    textSecond = textSecond + 'ы';
   }
 
-  if (array[day.getDay() - 1] == array[i]) {
-    out.innerHTML += `<b>${array[i]}</b><br>`;
+  if (m == 1 && minutes !== 11) {
+    textMinutes = textMinutes + 'а';
+  } else if (minutes == 12 || minutes == 13 || minutes == 14) {
+    textMinutes = textMinutes + '';
+  } else if (m >= 2 && m <= 4) {
+    textMinutes = textMinutes + 'ы';
   }
+
+  if ((hours >= 2 && hours <= 4) || (hours >= 22 && hours <= 24)) {
+    textHours = textHours + 'а';
+  }
+
+  if ((hours >= 5 && hours <= 20) || hours == 0) {
+    textHours = textHours + 'ов';
+  }
+
+  out.innerHTML = `Сегодня ${textDay} ${day} ${month} ${year} года 
+  ${hours} ${textHours} ${minutes} ${textMinutes} ${seconds} ${textSecond}`;
 }
+
+function getFullTime2() {
+  let nowDate = new Date();
+  let year = nowDate.getFullYear();
+  let month = zero(nowDate.getMonth() + 1);
+  let day = zero(nowDate.getDate());
+  let hours = zero(nowDate.getHours());
+  let minutes = zero(nowDate.getMinutes());
+  let seconds = zero(nowDate.getSeconds());
+
+  out1.innerText = `'${day}.${month}.${year} - ${hours}:${minutes}:${seconds}'`;
+}
+
+
+
+function zero(x) {
+  return (x < 10) ? '0' + x : x;
+}
+
+getFullTime1();
+getFullTime2();
+setInterval(getFullTime1, 1000);
+setInterval(getFullTime2, 1000);
